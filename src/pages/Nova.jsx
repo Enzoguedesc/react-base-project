206ff4416678f7f1a3e6150242d8621fc03fe19f
ENZO GUEDES CARDOSO <202401000019@alunos.ibmec.edu.br>
2024-10-03 17:08:15 -0300

    Aula de react

---
+++ src/pages/Nova.jsx
@@ -0,0 +1,11 @@
+import NovoComponente from "../components/NovoComponente/NovoComponente";
+
+const Nova = () => {
+    return (<>
+            <h1>Novo Título da Página</h1>
+            <p>oiiii</p>
+            <NovoComponente />
+        </>);
+}
+
+export default Nova;
